e804f9ef465c7ca854716ee89b1e6fdc68bc128a
Joseph Parsons <josephcpar@gmail.com>
2025-02-22 15:38:33 -0600

    Extract DateFilter component

---
+++ vue-project/src/components/DateFilter.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const yearValue = ref();
+const monthValue = ref();
+const dayValue = ref();
+const criterionTypeValue = ref("None");
+
+const criterionInvalid = computed(() => {
+  switch (criterionTypeValue.value) {
+    case "None":
+      return false;
+    case "Year":
+      return yearValue.value === undefined;
+    case "Month":
+      return monthValue.value === undefined;
+    case "Day":
+      return dayValue.value === undefined;
+    default:
+      return true;
+  }
+})
+
+function getCriterionInfo() {
+    let parts = undefined;
+    switch (criterionTypeValue.value) {
+        case "None":
+            return {
+                criterionType: criterionTypeValue.value,
+                criterion: {}
+            };
+        case "Year":
+            if (yearValue.value === undefined) return null
+            return {
+                criterionType: criterionTypeValue.value,
+                criterion: {
+                    year: yearValue.value
+                }
+            }
+        case "Month":
+            if (monthValue.value === undefined) return null
+            parts = monthValue.value.split("-")
+            return {
+                criterionType: criterionTypeValue.value,
+                criterion: {
+                    year: parseInt(parts[0]),
+                    month: parseInt(parts[1])
+                }
+            }
+        case "Day":
+            if (dayValue.value === undefined) return null
+            parts = dayValue.value.split("-");
+            return {
+                criterionType: criterionTypeValue.value,
+                criterion: {
+                    year: parseInt(parts[0]),
+                    month: parseInt(parts[1]),
+                    day: parseInt(parts[2])
+                }
+            }
+    }
+}
+
+</script>
+
+<template>
+    <div class="DateFilter-filter">
+        <p class="ubuntu-regular">Filter By:</p>
+        <select class="ubuntu-regular" v-model="criterionTypeValue">
+            <option key="None" class="ubuntu-regular">None</option>
+            <option key="Year" class="ubuntu-regular">Year</option>
+            <option key="Month" class="ubuntu-regular">Month</option>
+            <option key="Day" class="ubuntu-regular">Day</option>
+        </select>
+        <input type="number" class="ubuntu-regular DateFilter-year-input" v-model="yearValue" v-if="criterionTypeValue === 'Year'">
+        <input type="month" class="ubuntu-regular" v-model="monthValue" v-if="criterionTypeValue === 'Month'">
+        <input type="date" class="ubuntu-regular" v-model="dayValue" v-if="criterionTypeValue === 'Day'">
+        <button class="ubuntu-regular DateFilter-apply-filter" @click="$emit('applyFilter', getCriterionInfo())" :disabled="criterionInvalid">Apply Filter</button>
+    </div>
+</template>
+
+<style scoped>
+.DateFilter-filter {
+  display: flex;
+}
+.DateFilter-filter * {
+  margin-right: 0.5em;
+}
+.DateFilter-filter *:last-child {
+  margin-right: 0em;
+}
+.DateFilter-year-input {
+  width: 5em;
+}
+.DateFilter-apply-filter {
+  background-color: white;
+}
+</style>
--- vue-project/src/views/AllBankRecordsView.vue
+++ vue-project/src/views/AllBankRecordsView.vue
@@ -1,18 +1,14 @@
 <script setup>
-import { watch, ref, computed } from 'vue';
+import { watch, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import DataMessages from '@/components/DataMessages.vue';
 import useDataStore from '@/store/DataStore';
 import DateRecordList from '@/components/DateRecordList.vue';
+import DateFilter from '@/components/DateFilter.vue';
 
 const dataStore = useDataStore();
 const router = useRouter();
-
-const yearValue = ref();
-const monthValue = ref();
-const dayValue = ref();
-const criterionTypeValue = ref("None");
 
 const criterionType = ref("None");
 const criterion = ref(null);
@@ -30,59 +26,29 @@ function goToCreateRecordPage() {
   router.push("/create/record").then(dataStore.resetData);
 }
 
+function reloadData(criterionInfo) {
+  console.log(criterionInfo)
+  criterionType.value = criterionInfo.criterionType
+  criterion.value = criterionInfo.criterion
+  dataStore.resetData();
+}
+
 function loadData() {
-  let parts = undefined;
-  let yearPart = undefined;
-  let monthPart = undefined;
-  let dayPart = undefined;
-  switch (criterionTypeValue.value) {
+  switch (criterionType.value) {
     case "None":
-      criterionType.value = criterionTypeValue.value;
-      criterion.value = {};
       dataStore.loadDateAndBankRecords();
       break;
     case "Year":
-      if (yearValue.value === undefined) return;
-      criterionType.value = criterionTypeValue.value;
-      criterion.value = {year: yearValue.value};
       dataStore.loadDateAndBankRecordsDuringYear(criterion.value.year);
       break;
     case "Month":
-      if (monthValue.value === undefined) return;
-      parts = monthValue.value.split("-");
-      yearPart = parseInt(parts[0]);
-      monthPart = parseInt(parts[1]);
-      criterionType.value = criterionTypeValue.value;
-      criterion.value = {year: yearPart, month: monthPart};
       dataStore.loadDateAndBankRecordsDuringMonth(criterion.value.year, criterion.value.month);
       break;
     case "Day":
-      if (dayValue.value === undefined) return;
-      parts = dayValue.value.split("-");
-      yearPart = parseInt(parts[0]);
-      monthPart = parseInt(parts[1]);
-      dayPart = parseInt(parts[2]);
-      criterionType.value = criterionTypeValue.value;
-      criterion.value = {year: yearPart, month: monthPart, day: dayPart};
       dataStore.loadDateAndBankRecordsDuringDay(criterion.value.year, criterion.value.month, criterion.value.day);
       break;
   }
 }
-
-const criterionInvalid = computed(() => {
-  switch (criterionTypeValue.value) {
-    case "None":
-      return false;
-    case "Year":
-      return yearValue.value === undefined;
-    case "Month":
-      return monthValue.value === undefined;
-    case "Day":
-      return dayValue.value === undefined;
-    default:
-      return true;
-  }
-})
 
 async function populateData() {
   await dataStore.populateDataAsync();
@@ -92,19 +58,7 @@ async function populateData() {
 <template>
   <h1 class="libre-baskerville-regular">Records</h1>
   <a @click="goToCreateRecordPage" class="libre-baskerville-regular AllBankRecordsView-add-button">Add New Bank Record</a>
-  <div class="AllBankRecordsView-filter">
-    <p class="ubuntu-regular">Filter By:</p>
-    <select class="ubuntu-regular" v-model="criterionTypeValue">
-      <option key="None" class="ubuntu-regular">None</option>
-      <option key="Year" class="ubuntu-regular">Year</option>
-      <option key="Month" class="ubuntu-regular">Month</option>
-      <option key="Day" class="ubuntu-regular">Day</option>
-    </select>
-    <input type="number" class="ubuntu-regular AllBankRecordsView-year-input" v-model="yearValue" v-if="criterionTypeValue === 'Year'">
-    <input type="month" class="ubuntu-regular" v-model="monthValue" v-if="criterionTypeValue === 'Month'">
-    <input type="date" class="ubuntu-regular" v-model="dayValue" v-if="criterionTypeValue === 'Day'">
-    <button class="ubuntu-regular AllBankRecordsView-apply-filter" @click="dataStore.resetData" :disabled="criterionInvalid">Apply Filter</button>
-  </div>
+  <DateFilter @applyFilter="reloadData" />
   <button @click="populateData">Populate Data</button>
   <DataMessages :retrievalStatus="dataStore.retrievalStatus"
   loadingMessage="Loading Records..." errorMessage="Could Not Load Records">
@@ -118,20 +72,5 @@ async function populateData() {
   color: #050;
   display: block;
   text-align: center;
-}
-.AllBankRecordsView-filter {
-  display: flex;
-}
-.AllBankRecordsView-filter * {
-  margin-right: 0.5em;
-}
-.AllBankRecordsView-filter *:last-child {
-  margin-right: 0em;
-}
-.AllBankRecordsView-year-input {
-  width: 5em;
-}
-.AllBankRecordsView-apply-filter {
-  background-color: white;
 }
 </style>
